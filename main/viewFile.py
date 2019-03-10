fbeee5fdaccd9d23180d8029349567e79af25ad2
Alexander <alex20k.x@gmail.com>
2019-03-10 16:58:17 +0600

    Added Documentation and comment

--- main/viewFile.py
+++ main/viewFile.py
@@ -17,8 +17,8 @@ from modelsFile import Product
 import os
 
 
-# Writing binary data to file
 def write_file(data, filename):
+    """Writing binary data to file"""
     with open(filename, 'wb') as f:
         f.write(data)
 
@@ -26,8 +26,8 @@ def write_file(data, filename):
 def make_dir_image(fileimage, foldername):
     img_title = fileimage.filename
     dirstr = "static/" + foldername
-    os.mkdir(dirstr)
-    write_file(fileimage.read(), dirstr + "/" + img_title)
+    os.mkdir(dirstr)  # Creating a folder
+    write_file(fileimage.read(), dirstr + "/" + img_title)  # Write a photo to its associated folder
 
 
 @app.route('/')  # The user can appeal the web application on the path '/'  {'/' : 'welcome_page'}
@@ -78,17 +78,13 @@ def add_page():
 
         make_dir_image(customimg, customheadline)
 
-        if customheadline:
-            new_product = Product(headline=customheadline, text=customtext, description=customdescription,
+        new_product = Product(headline=customheadline, text=customtext, description=customdescription,
                                   cost=customcost, img_title=img_title)
 
-            db.session.add(new_product)
-            db.session.commit()
+        db.session.add(new_product)
+        db.session.commit()
 
-            return redirect(url_for('welcome_page'))
-
-        else:
-            return 'Headline - empty!'
+        return redirect(url_for('welcome_page'))
 
     adding_products_forms = ProductsAddingForms()
     return render_template('products_add.html', adding_products_forms=adding_products_forms)
